ee89d2bb769d063de1c7980fd964cc1bcfd776b6
Replit user <>
2023-03-09 16:24:24 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -84,7 +84,7 @@ typedef struct {
 Vector *crearVector(int n) {
    Vector arrayTmp;
    arrayTmp.capacidad = n;
-   arrayTmp -> datos = NULL;
+   arrayTmp -> &datos = NULL;
 
    //arrayTmp -> datos = malloc(arrayTmp.capacidad * sizeof(int));
 
